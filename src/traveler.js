43e8f88eb1e220e9dcc9033cb372abdf946747fc
Melanie Daoheuang <daoh0031@gmail.com>
2021-04-23 21:15:57 -0600

    Remove useless unit tests

--- src/traveler.js
+++ src/traveler.js
@@ -5,24 +5,7 @@ class Traveler {
     this.id = userData.id;
     this.name = userData.name;
     this.travelerType = userData.travelerType;
-    // this.userTrips = 
-    
-  }
-
-  //method for all trips of just that user
-  findPastTrips(date) {
-  
-  }
-
-  findCurrentTrip() {
-
-  }
-
-  findUpcomingTrips() {
-
   }
 }
-
-// Methods: filter trip ( past, present, upcoming and pending) >> Total amt spent on trips this year + 10% agent fee 
 
 export default Traveler;
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -26,31 +26,4 @@ describe('Traveler', () => {
   it('should have a traveler type', () => {
     expect(user1.travelerType).to.equal("relaxer");
   });
-
-  it.skip('should filter past trips', () => {
-    expect(user1.findPastTrips(date)).to.equal([
-      {"date": "2020/01/19", "name": "Lima, Peru"}
-    ]);
-  });
-
-  it.skip('should filter present trips', () => {
-    expect(user1.findCurrentTrip()).to.equal({"date": "2020/5/04", "name": "Stockholm, Sweden"});
-  });
-
-  it.skip('should filter upcoming trips', () => {
-    expect(user1.findUpcomingTrips()).to.equal([
-      {"date": "2020/10/22", "name": "Sydney, Austrailia"}
-    ]);
-  });
-
-  it.skip('should filter pending trips', () => {
-    expect(user1.findPendingTrips()).to.equal([
-      {"date": "2020/10/22", "name": "Sydney, Austrailia"}
-    ]);
-  });
-
-  it.skip('should calculate the total amount spent on trips for the current year', () => {
-    expect(user1.calculateAnnualSpent()).to.equal("relaxer");
-  });
-
 });
