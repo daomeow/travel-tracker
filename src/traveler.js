34a2304c3e50fdd7d5fcab80b9b77d180b4180f2
Melanie Daoheuang <daoh0031@gmail.com>
2021-04-27 11:02:34 -0600

    Update traveler class

--- src/traveler.js
+++ src/traveler.js
@@ -1,11 +1,6 @@
 class Traveler {
   constructor(travelersData) {
     this.allTravelers = travelersData.travelers;
-    // this.currentTravelerData = this.findCurrentTraveler();
-    // console.log(this.currentTravelerData)
-    // this.id = this.currentTravelerData.id;
-    // this.name = this.findCurrentTraveler().name;
-    // this.travelerType = this.findCurrentTraveler().travelerType;
   }
 
   findCurrentTraveler(travelerID) {
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import {fakeUsers} from '../src/data/fakeData';
 import Traveler from '../src/traveler.js';
 
@@ -14,42 +14,12 @@ describe('Traveler', () => {
   it('should be an instance of Traveler', () => {
     expect(newTraveler).to.be.an.instanceOf(Traveler);
   });
+  
+  it('should contain all the travelers', () => {
+    expect(newTraveler.allTravelers).to.deep.equal(fakeUsers.travelers);
+  });
 
   it('should be able to identify the traveler based of their ID', () => {
     expect(newTraveler.findCurrentTraveler(1)).to.deep.equal(user1)
   });
-
-  it.skip('should have a user id', () => {
-    expect(newTraveler.id).to.equal(1);
-  });
-
-  it.skip('should have a user name', () => {
-    expect(user1.name).to.equal("Ham Leadbeater");
-  });
-
-  it.skip('should have a traveler type', () => {
-    expect(user1.travelerType).to.equal("relaxer");
-  });
-
-  // beforeEach(() => {
-  //   user1 = new Traveler(fakeUsers.travelers[0]);
-  // })
-
-  // it('should be an instance of Traveler', () => {
-  //   expect(user1).to.be.an.instanceOf(Traveler);
-  // });
-
-  // it('should have a user id', () => {
-  //   expect(user1.id).to.equal(1);
-  // });
-
-  // it('should have a user name', () => {
-  //   expect(user1.name).to.equal("Ham Leadbeater");
-  // });
-
-  // it('should have a traveler type', () => {
-  //   expect(user1.travelerType).to.equal("relaxer");
-  // });
-
-
 });
